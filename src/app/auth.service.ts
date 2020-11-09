12ae9e9284810284edf86ac178a8ccfe0357b1c1
el mehdi rhindi <g6handi@gmail.com>
2020-11-09 14:10:24 +0100

    add refresh token

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,13 +3,14 @@ import { WebRequestService } from './web-request.service'
 import { Router } from '@angular/router'
 import { shareReplay, tap } from 'rxjs/operators'
 import { HttpResponse } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private webReqSer: WebRequestService, private router: Router) { }
+  constructor(private webReqSer: WebRequestService, private router: Router, private http: HttpClient) { }
 
   login(email: string, password: string) {
     return this.webReqSer.login(email, password).pipe(
@@ -22,6 +23,7 @@ export class AuthService {
 
   logout() {
     this.removeSession()
+    this.router.navigateByUrl('/login')
   }
 
   getAccessToken() {
@@ -30,6 +32,20 @@ export class AuthService {
 
   getRefreshToken() {
     return localStorage.getItem('refresh-token')
+  }
+
+  getUserId() {
+    return localStorage.getItem('user-id')
+  }
+
+  getRefreshOptions() {
+    return {
+      headers: {
+        'x-refresh-token': this.getRefreshToken(), 
+        '_id': this.getUserId()
+      },
+      observe: 'response'
+    }
   }
 
   setAccessToken(accessToken: string) {
@@ -46,5 +62,19 @@ export class AuthService {
     localStorage.removeItem('user-id')
     localStorage.removeItem('access-token')
     localStorage.removeItem('refresh-token')
+  }
+
+  getNewAccessToken() {
+    return this.http.get(`${this.webReqSer.URL}/users/me/access-token`, {
+      headers: {
+        'x-refresh-token': this.getRefreshToken(), 
+        '_id': this.getUserId()
+      },
+      observe: 'response'
+    }).pipe(
+      tap(res => {
+        this.setAccessToken(res.headers.get('x-access-token'))
+      })
+    )
   }
 }
--- src/app/web-request-interceptor.service.ts
+++ src/app/web-request-interceptor.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
-import { Observable, throwError } from 'rxjs';
+import { empty, Observable, throwError } from 'rxjs'
 import { AuthService } from './auth.service'
-import { catchError } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators'
+import { tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebRequestInterceptorService implements HttpInterceptor {
+  refreshingAccessToken: boolean
 
   constructor(private authService: AuthService) { }
 
@@ -15,8 +17,31 @@ export class WebRequestInterceptorService implements HttpInterceptor {
     request = this.addAuthHeader(request)
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log('interceptor error', error)
+        if (error.status === 401 && !this.refreshingAccessToken) {
+          return this.refreshAccessToken()
+          .pipe(
+            switchMap(() => {
+              request = this.addAuthHeader(request)
+              return next.handle(request)
+            }),
+            catchError(err => {
+              console.log(err)
+              this.authService.logout()
+              return empty()
+            })
+          )
+        }
         return throwError(error)
+      })
+    )
+  }
+
+  refreshAccessToken() {
+    this.refreshingAccessToken = true
+    return this.authService.getNewAccessToken().pipe(
+      tap(res => {
+        this.refreshingAccessToken = false
+        console.log('refreshed', res)
       })
     )
   }
